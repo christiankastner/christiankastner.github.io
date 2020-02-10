fd6b30bd5bdc2f9f40b1798c3e3b7fa269754600
Christian Kastner <christianmkastner@gmail.com>
2020-02-10 15:32:51 -0800

    Adding project sections

--- index.js
+++ index.js
@@ -11,32 +11,31 @@ const PROJECTS = [
         meta: "Interactive Audio Visualizer",
         description: "A shared space for audio and visual artists to create on the same canvas simultaneously. By combining the animation browser animation and audio with P5.js, UI state management with React Redux, and websocketing with ActionCable in rails, Virtual Canvas allows users to create shapes synced to audio and collaborate on a music visualizer together.",
         demo: "",
-        link: ""
+        active: "",
     },
     {
         title: "Kangaroo Hangout",
-        img: null,
+        img: "./public/hangout.png",
         meta: "Event Planning App",
         description: "Want to plan a whole day's outing? Kangaroo Hangout is an event planning app that lets user's input places they'd like to go to, then search for nearby locations to also do. Want Sushi after your Muay Thai class or a library near your favorite burger place? That's easy with kangaroo hangout. Places are found using google's Places API that are persisted to a Rails backend and served to a React frontend.",
         demo: "",
-        link: ""
+        active: ""
     },
     {
-        title: "Geek Poems",
-        img: null,
-        meta: "",
+        title: "Beat Poems",
+        img: "./public/poems.png",
+        meta: "Natural Language Processing",
         description: "A single page app that makes your poems a little less ordinary. Users can input or write poems that made ",
         demo: "",
-        link: ""
+        active: ""
     }
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("you made it!");
-
     const about = document.getElementById("text-card");
-    console.log(about);
+    const projectList = document.getElementById("project-list");
 
+    // Populate about section
     (function() {
         ABOUT.map(paragraph => {
             p = document.createElement("p")
@@ -44,4 +43,35 @@ document.addEventListener('DOMContentLoaded', () => {
             about.appendChild(p)
         })
     })();
+
+    // Populate project list
+    (function() {
+        PROJECTS.map(project => {
+            li = document.createElement("li")
+            li.className = "project-card"
+
+            img = document.createElement("img")
+            img.src = project.img
+            img.alt = `${project.title} picture`
+
+            div = document.createElement("div")
+            div.className = "project-header"
+
+            h3 = document.createElement("h3")
+            h3.innerText = project.title
+            h4 = document.createElement("h4")
+            h4.innerText = project.meta
+            div.append(h3,h4)
+
+            p = document.createElement("p")
+            p.innerText = project.description
+
+            li.append(img,div,p)
+            projectList.appendChild(li)
+        })
+    })();
+
+    // Populate Skills
+
+    
 })
